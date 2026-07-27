258c72f76a3112c412e34161bd418684ea751043
Igor Melnik <olesukdavid795@gmail.com>
2026-07-28 00:49:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-function calculate() {
+function calculate(){
 
-let alcohol =
-(Number(document.getElementById("a2").value) * 900) +
-(Number(document.getElementById("a3").value) * 1200);
-
-
-let petrushka =
-(Number(document.getElementById("p2").value) * 800) +
-(Number(document.getElementById("p3").value) * 1100);
+let a2 = Number(a2.value) * Number(a2price.value);
+let a3 = Number(document.getElementById("a3").value) *
+Number(document.getElementById("a3price").value);
 
 
-let total = alcohol + petrushka;
+let p2 = Number(document.getElementById("p2").value) *
+Number(document.getElementById("p2price").value);
+
+let p3price = Number(document.getElementById("p3price").value);
+
+if(p3price > 1100){
+p3price = 1100;
+}
+
+let p3 = Number(document.getElementById("p3").value) * p3price;
+
+
+let total = a2 + a3 + p2 + p3;
 
 
 document.getElementById("result").innerHTML =
 `
-🍾 Алкоголь: ${alcohol} грн <br>
-🌿 Петрушка: ${petrushka} грн <br><br>
-
+🍾 Алкоголь: ${a2+a3} грн <br>
+🌿 Петрушка: ${p2+p3} грн <br><br>
 🔥 Всього: ${total} грн
 `;
 
