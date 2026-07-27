c95739483942100ddeb978d1f6720560cb0b92b0
Igor Melnik <olesukdavid795@gmail.com>
2026-07-28 01:16:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function calculate(){
-let a2 = Number(document.getElementById("a2").value) *
-Number(document.getElementById("a2price").value);
+
+let a2price = 900;
+
+let a2 = Number(document.getElementById("a2").value) * a2price;
+
+
 let a3price = Number(document.getElementById("a3price").value);
 
 if(a3price > 1200){
@@ -10,13 +14,19 @@ if(a3price > 1200){
 let a3 = Number(document.getElementById("a3").value) * a3price;
 
 
-let p2 = Number(document.getElementById("p2").value) *
-Number(document.getElementById("p2price").value);
+let p2price = Number(document.getElementById("p2price").value);
+
+if(p2price > 800){
+    p2price = 800;
+}
+
+let p2 = Number(document.getElementById("p2").value) * p2price;
+
 
 let p3price = Number(document.getElementById("p3price").value);
 
 if(p3price > 1100){
-p3price = 1100;
+    p3price = 1100;
 }
 
 let p3 = Number(document.getElementById("p3").value) * p3price;
